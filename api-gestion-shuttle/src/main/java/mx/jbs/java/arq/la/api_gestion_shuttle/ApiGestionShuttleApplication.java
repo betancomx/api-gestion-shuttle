6c2be73402476aab530fd8ce8715303f59af1a55
BetancoMx <betancomx@hotmail.com>
2026-02-11 12:03:37 -0600

    Estructura inicial de archivos

---
+++ api-gestion-shuttle/src/main/java/mx/jbs/java/arq/la/api_gestion_shuttle/ApiGestionShuttleApplication.java
@@ -0,0 +1,13 @@
+package mx.jbs.java.arq.la.api_gestion_shuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionShuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionShuttleApplication.class, args);
+	}
+
+}
---
+++ api-gestion-shuttle/src/test/java/mx/jbs/java/arq/la/api_gestion_shuttle/ApiGestionShuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.jbs.java.arq.la.api_gestion_shuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionShuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
